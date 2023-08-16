52bbffe5e1bfa9b7979c3be3216c662fd7323613
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-08-16 20:39:28 +0700

    6-2

--- Homework/HW06/hw06-2-roman-number.cpp
+++ Homework/HW06/hw06-2-roman-number.cpp
@@ -23,3 +23,34 @@
         929 = CMXXIX
 
 */
+#include <stdio.h>
+
+// สัญลักษณ์และค่าที่เกี่ยวข้องกับตัวเลขโรมัน
+char *roman_numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+int arabic_values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+void convertToRoman(int number) {
+    if (number <= 0) {
+        printf("Input must be a positive integer.\n");
+        return;
+    }
+
+    printf("%d = ", number);
+
+    for (int i = 0; number > 0; i++) {
+        while (number >= arabic_values[i]) {
+            printf("%s", roman_numerals[i]);
+            number -= arabic_values[i];
+        }
+    }
+
+    printf("\n");
+}
+
+int main() {
+    int input;
+    printf("Enter a number: ");
+    scanf("%d", &input);
+    convertToRoman(input);
+    return 0;
+}
